43d6e4238d88cd570195223dfc121cdc3b5412e1
Gregwar <g.passault@gmail.com>
2014-11-26 16:38:44 +0100

    dxl

--- firmware/libraries/DynaBan/dxl.cpp
+++ firmware/libraries/DynaBan/dxl.cpp
@@ -3,16 +3,107 @@
 #include <string.h>
 #include <wirish/wirish.h>
 #include "dxl.h"
+#include "flash_write.h"
+
+const char dxl_zone[1024] __attribute__((section("eeprom"), aligned(1024))) = {0};
 
 struct dxl_registers dxl_regs;
-struct dxl_packet dxl_rxpacket;
+struct dxl_packet dxl_packet;
+static ui8 dxl_buffer[DXL_BUFFER_SIZE];
 
-volatile int dxl_debug = 0;
+void dxl_persist()
+{
+    flashWrite((int)dxl_zone, (void*)&dxl_regs.eeprom, sizeof(struct dxl_eeprom));
+}
+
+void dxl_tick()
+{
+    if (!dxl_sending()) {
+        while (dxl_data_available()) {
+            dxl_push_byte(dxl_data_byte());
+
+            if (dxl_packet.process) {
+                dxl_process();
+
+                if (dxl_packet.answer) {
+                    int n = dxl_write(dxl_buffer);
+                    dxl_send(dxl_buffer, n);
+                }
+            }
+        }
+    }
+
+    if (dxl_regs.eeprom_dirty) {
+        dxl_regs.eeprom_dirty = false;
+        dxl_persist();
+    }
+}
+
+void dxl_init()
+{
+    dxl_init_regs();
+    dxl_packet_init(&dxl_packet);
+}
+
+void dxl_init_regs()
+{
+    struct dxl_eeprom *dxl_flash = (struct dxl_eeprom *)dxl_zone;
+
+    if (dxl_flash->modelNumber == DXL_MODEL) {
+        memcpy((void*)&dxl_regs.eeprom, (void*)dxl_flash, sizeof(struct dxl_eeprom));
+    } else {
+        dxl_regs.eeprom.modelNumber = DXL_MODEL;
+        dxl_regs.eeprom.firmwareVersion = 36;
+        dxl_regs.eeprom.id = 1;
+        dxl_regs.eeprom.baudrate = 34;
+        dxl_regs.eeprom.returnDelay = 250;
+        dxl_regs.eeprom.cwLimit = 0;
+        dxl_regs.eeprom.ccwLimit = 0xfff;
+        dxl_regs.eeprom.temperatureLimit = 80;
+        dxl_regs.eeprom.lowestVoltage = 60;
+        dxl_regs.eeprom.highestVoltage = 160;
+        dxl_regs.eeprom.maxTorque = 0x3ff;
+        dxl_regs.eeprom.returnStatus = 2;
+        dxl_regs.eeprom.alarmLed = 36;
+        dxl_regs.eeprom.alarmShutdown = 36;
+        dxl_regs.eeprom.multiTurnOffset = 0;
+        dxl_regs.eeprom.resolutionDivider = 1;
+        dxl_persist();
+    }
+
+    dxl_regs.ram.torqueEnable = 0;
+    dxl_regs.ram.led = 0;
+    dxl_regs.ram.servoKd = 0;
+    dxl_regs.ram.servoKi = 0;
+    dxl_regs.ram.servoKp = 32;
+    dxl_regs.ram.torqueLimit = dxl_regs.eeprom.maxTorque;
+    dxl_regs.ram.registeredInstruction = 0;
+    dxl_regs.ram.moving = 0;
+    dxl_regs.ram.lock = 0;
+    dxl_regs.ram.punch = 0;
+    dxl_regs.ram.current = 0;
+    dxl_regs.ram.torqueMode = 0;
+    dxl_regs.ram.goalTorque = 0;
+    dxl_regs.ram.goalAcceleration = 0;
+
+    dxl_regs.eeprom_dirty = false;
+}
+
+void dxl_push_byte(ui8 b)
+{
+    dxl_packet_push_byte(&dxl_packet, b);
+}
+
+int dxl_write(ui8 *buffer)
+{
+    return dxl_write_packet(&dxl_packet, buffer);
+}
 
 void dxl_packet_init(volatile struct dxl_packet *packet)
 {
     packet->dxl_state = 0;
     packet->process = false;
+    packet->answer = false;
 }
 
 /**
@@ -45,13 +136,13 @@ ui8 dxl_compute_checksum(volatile struct dxl_packet *packet) {
     sum += packet->id;
     sum += packet->instruction;
     sum += packet->parameter_nb+2;
-    
+
     for (i=0; i<packet->parameter_nb; i++) {
         sum += packet->parameters[i];
     }
 
     sum = ~(sum & 0xFF);
-    
+
     return (ui8) sum;
 }
 
@@ -68,12 +159,16 @@ void dxl_packet_push_byte(volatile struct dxl_packet *packet, ui8 b)
             packet->id = b;
             break;
         case 3:
+            if (b < 2) {
+                goto pc_error;
+            }
             packet->parameter_nb = b - 2;
             break;
         case 4:
             packet->instruction = b;
             break;
         default:
+            digitalWrite(BOARD_LED_PIN, HIGH);
             if (packet->dxl_state - 4 > packet->parameter_nb) {
                 goto pc_ended;
 
@@ -85,17 +180,90 @@ void dxl_packet_push_byte(volatile struct dxl_packet *packet, ui8 b)
                 goto pc_error;
             }
     }
-        
+
     packet->dxl_state++;
     return;
 
 pc_ended:
+    digitalWrite(BOARD_LED_PIN, LOW);
     if (dxl_compute_checksum(packet) == b) {
         packet->process = true;
+        packet->answer = false;
     }
-
+    
     packet->dxl_state = 0;
     return;
+
 pc_error:
     packet->dxl_state = 0;
+}
+
+static void dxl_write_data(ui8 addr, ui8 *values, ui8 length)
+{
+    memcpy(((ui8 *)(&dxl_regs))+addr, values, length);
+
+    if (addr < DXL_RAM_BEGIN) {
+        dxl_regs.eeprom_dirty = true;
+    }
+}
+
+static void dxl_read_data(ui8 addr, ui8 *values, ui8 length, ui8 *error)
+{
+    memcpy(values, ((ui8*)&dxl_regs)+addr, length);
+}
+
+void dxl_process()
+{
+    dxl_packet.process = false;
+    dxl_packet.answer = false;
+
+    if (dxl_regs.eeprom.id == dxl_packet.id || dxl_packet.id == DXL_BROADCAST) {
+        switch (dxl_packet.instruction) {
+            case DXL_PING: 
+                // Answers the ping
+                if (dxl_packet.id != DXL_BROADCAST) {
+                    dxl_packet.error = DXL_NO_ERROR;
+                    dxl_packet.parameter_nb = 0;
+                    dxl_packet.answer = true;
+                }
+                break;
+            case DXL_WRITE_DATA:
+                // Write data
+                dxl_write_data(dxl_packet.parameters[0],
+                        (ui8 *)&dxl_packet.parameters[1],
+                        dxl_packet.parameter_nb-1);
+                break;
+
+            case DXL_SYNC_WRITE: {
+                                     ui8 addr = dxl_packet.parameters[0];
+                                     int length = dxl_packet.parameters[1] + 1;
+                                     int K = (dxl_packet.parameter_nb-2) / length;
+                                     int i;
+
+                                     for (i=0; i<K; i++) {
+                                         if (dxl_packet.parameters[2+i*length] == dxl_regs.eeprom.id) {
+                                             dxl_write_data(addr,
+                                                     (ui8 *)&dxl_packet.parameters[2+i*length+1],
+                                                     (ui8)(length-1));
+                                         }
+                                     }
+                                 }
+                                 break;
+
+            case DXL_READ_DATA:
+                                 // Read some data
+                                 if (dxl_packet.id != DXL_BROADCAST) {
+                                     ui8 addr = dxl_packet.parameters[0];
+                                     unsigned int length = dxl_packet.parameters[1];
+                                     dxl_packet.answer = true;
+
+                                     if (length < sizeof(dxl_packet.parameters)) {
+                                         dxl_read_data(addr, (ui8 *)dxl_packet.parameters, 
+                                                 length, (ui8 *)&dxl_packet.error);
+
+                                         dxl_packet.parameter_nb = length;
+                                     }
+                                 }
+        }
+    }
 }
--- firmware/libraries/DynaBan/dxl.h
+++ firmware/libraries/DynaBan/dxl.h
@@ -10,6 +10,21 @@
 // Maximum parameters in a packet
 #define DXL_MAX_PARAMS 140
 
+// Address of dynamixel first RAM register
+#define DXL_RAM_BEGIN   0x18
+
+// Model number
+#define DXL_MODEL       29
+
+#define DXL_PING        0x01
+#define DXL_READ_DATA   0x02
+#define DXL_WRITE_DATA  0x03
+#define DXL_REG_WRITE   0x04
+#define DXL_ACTION      0x05
+#define DXL_RESET       0x06
+#define DXL_SYNC_WRITE  0x83
+#define DXL_NO_ERROR    0x0
+
 typedef unsigned char ui8;
 
 /**
@@ -24,62 +39,90 @@ struct dxl_packet {
     ui8 parameter_nb;
     ui8 parameters[DXL_MAX_PARAMS];
     bool process;
+    bool answer;
     ui8 dxl_state;
 };
+
+// HAL to implement
+unsigned int dxl_data_available();
+ui8 dxl_data_byte();
+void dxl_send(ui8 *buffer, int n);
+bool dxl_sending();
+
+// Call init to init the dynamixel structures, and then tick it periodically
+void dxl_init();
+void dxl_tick();
+
+void dxl_process();
+void dxl_init_regs();
+void dxl_push_byte(ui8 b);
+int dxl_write(ui8 *buffer);
 
 void dxl_packet_init(volatile struct dxl_packet *packet);
 void dxl_packet_push_byte(volatile struct dxl_packet *packet, ui8 b);
 int dxl_write_packet(volatile struct dxl_packet *packet, ui8 *buffer);
 ui8 dxl_compute_checksum(volatile struct dxl_packet *packet);
+    
+struct dxl_eeprom {
+    unsigned short modelNumber;             // 0x00
+    unsigned char firmwareVersion;          // 0x02
+    unsigned char id;                       // 0x03
+    unsigned char baudrate;                 // 0x04
+    unsigned char returnDelay;              // 0x05
+    unsigned short cwLimit;                 // 0x06
+    unsigned short ccwLimit;                // 0x08
+    unsigned char _padding;                 // 0x0a
+    unsigned char temperatureLimit;         // 0x0b
+    unsigned char lowestVoltage;            // 0x0c
+    unsigned char highestVoltage;           // 0x0d
+    unsigned short maxTorque;               // 0x0e
+    unsigned char returnStatus;             // 0x10
+    unsigned char alarmLed;                 // 0x11
+    unsigned char alarmShutdown;            // 0x12
+    unsigned char _padding6;                // 0x13
+    unsigned short multiTurnOffset;         // 0x14
+    unsigned char resolutionDivider;        // 0x16
+    volatile unsigned char _padding2;       // 0x17
+} __attribute__((packed)); // Size: 24
+    
+struct dxl_ram {
+    unsigned char torqueEnable;             // 0x18
+    unsigned char led;                      // 0x19
+    unsigned char servoKd;                  // 0x1a
+    unsigned char servoKi;                  // 0x1b
+    unsigned char servoKp;                  // 0x1c
+    unsigned char _padding7;                // 0x1d
+    unsigned short goalPosition;            // 0x1e
+    unsigned short movingSpeed;             // 0x20
+    unsigned short torqueLimit;             // 0x22
+    unsigned short presentPosition;         // 0x24
+    unsigned short presentSpeed;            // 0x26
+    unsigned short presentLoad;             // 0x28
+    unsigned char presentVoltage;           // 0x2a
+    unsigned char presentTemperature;       // 0x2b
+    unsigned char registeredInstruction;    // 0x2c
+    unsigned char _padding3;                // 0x2d
+    unsigned char moving;                   // 0x2e
+    unsigned char lock;                     // 0x2f
+    unsigned short punch;                   // 0x30
+    unsigned char _padding4[18];            // 0x32
+    unsigned short current;                 // 0x44
+    unsigned char torqueMode;               // 0x46
+    unsigned short goalTorque;              // 0x47
+    unsigned char goalAcceleration;         // 0x49
+} __attribute__((packed));
 
 struct dxl_registers
 {
-    volatile struct dxl_eeprom {
-        unsigned short  modelNumber;
-        unsigned char firmwareVersion;
-        unsigned char id;
-        unsigned char baudrate;
-        unsigned char returnDelay;
-        unsigned short cwLimit;
-        unsigned short ccwLimit;
-        unsigned char _dummy;
-        unsigned char temperatureLimit;
-        unsigned char lowestVoltage;
-        unsigned char highestVoltage;
-        unsigned short maxTorque;
-        unsigned char returnStatus;
-        unsigned char alarmLed;
-        unsigned char alarmShutdown;
-    } eeprom __attribute((packed));
-
-    volatile unsigned char _dummy2[4]; 
-
-    volatile struct dxl_ram {
-        unsigned char torqueEnable;
-        unsigned char led;
-        unsigned char cwComplianceMargin;
-        unsigned char ccwComplianceMargin;
-        unsigned char cwComplianceSlope;
-        unsigned char ccwComplianceSlope;
-        unsigned short goalPosition;
-        unsigned short movingSpeed;
-        unsigned short torqueLimit;
-        unsigned short presentPosition;
-        unsigned short presentSpeed;
-        unsigned short presentLoad;
-        unsigned char presentVoltage;
-        unsigned char presentTemperature;
-        unsigned char registeredInstruction;
-        unsigned char _dummy3;
-        unsigned char moving;
-        unsigned char lock;
-        unsigned short punch;
-    } ram __attribute__((packed));
-
+    volatile struct dxl_eeprom eeprom;
+    volatile struct dxl_ram ram;
     volatile char eeprom_dirty;
 } __attribute__((packed));
 
+// Dynamixel registers
 extern struct dxl_registers dxl_regs;
-extern struct dxl_packet    dxl_rxpacket;
+
+// Dynamixel packets
+extern struct dxl_packet    dxl_packet;
 
 #endif // DXL_H
